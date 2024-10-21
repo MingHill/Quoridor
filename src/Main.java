0d8bdaac8c3f277665e061489525ede177f23b2d
Ming <soongming18@gmail.com>
2024-10-20 23:01:42 -0400

    changed game shop name

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 package src;
 
+
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.util.Scanner;
 
+
+
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         src.Misc.AudioPlayer audio = new src.Misc.AudioPlayer("./assets/220870__diboz__ibrkr01.wav");
         audio.loop();
-        System.out.println("\nWelcome to Ming's Game Shop! We have three game to play today");
+        System.out.println("\nWelcome to Ming and Niten's Game Shop! We have three game to play today");
         boolean quit = false;
         while(!quit){
 
